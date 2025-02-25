71d54b499cdc0e6d9b7b88bcc6b4ce5e6f6bfa0b
DQE92 <duquedylan@icloud.com>
2025-02-25 10:58:30 +0100

    add models

---
+++ part2/hbnb/app/models/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.review import Review
+from models.amenity import Amenity
+
+__all__ = [
+    'BaseModel',
+    'User',
+    'Place',
+    'Review',
+    'Amenity'
+]
---
+++ part2/hbnb/app/models/amenity.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
+
+class Amenity(BaseModel):
+    def __init__(self, name: str):
+        super().__init__()
+        self.validate_name(name)
+        self.name = name
+
+    @staticmethod
+    def validate_name(name: str):
+        if not name or len(name) > 50:
+            raise ValueError("Name must be between 1 and 50 characters")
