b558bd8ec5b7d78fd749516e96aebd3bfdad3914
Lucawinwin <windwehrluca@gmail.com>
2025-03-11 11:36:28 +0100

    password

--- part2/app/models/user.py
+++ part2/app/models/user.py
@@ -12,7 +12,13 @@ class User(BaseModel):
         self.last_name = last_name
         self.email = email
         self.is_admin = is_admin
-
+        
+        def hash_password(self, password):
+    """Hashes the password before storing it."""
+    self.password = bcrypt.generate_password_hash(password).decode('utf-8')
+        def verify_password(self, password):
+    """Verifies if the provided password matches the hashed password."""
+    return bcrypt.check_password_hash(self.password, password)
 
 @property
 def first_name(self):
