3367ce91d9ac0cb8545e132c1e065169c32ae70c
Lucawinwin <windwehrluca@gmail.com>
2025-03-11 11:50:51 +0100

    password

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -5,12 +5,16 @@ from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
 from flask_bcrypt import Bcrypt
+from config import DevelopmentConfig
 
-def create_app():
+def create_app(config_class=config.DevelopmentConfig):
+    db.init_app(app)
     app = Flask(__name__)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
     bcrypt = Bcrypt()
     bcrypt.init_app(app)
+    app.config.from_object(config_class
+    app.config.from_object(config_class))
     # Register the users namespace
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
