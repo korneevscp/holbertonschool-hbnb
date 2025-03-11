bf9d861ca23546b4898391882031c27a1a97036c
DQE92 <duquedylan@icloud.com>
2025-03-11 13:36:50 +0100

    add task 00

--- part3/app/services/facade.py
+++ part3/app/services/facade.py
@@ -11,20 +11,24 @@ class HBnBFacade:
         self.review_repo = InMemoryRepository()
         self.amenity_repo = InMemoryRepository()
 
-#User
+class HBnBFacade:
+    def __init__(self):
+        self.user_repository = SQLAlchemyRepository(User)  # Switched to SQLAlchemyRepository
+        self.place_repository = SQLAlchemyRepository(Place)
+        self.review_repository = SQLAlchemyRepository(Review)
+        self.amenity_repository = SQLAlchemyRepository(Amenity)
+
+#user
     def create_user(self, user_data):
         user = User(**user_data)
-        self.user_repo.add(user)
+        self.user_repository.add(user)
         return user
 
-    def get_user(self, user_id):
-        return self.user_repo.get(user_id)
-
-    def get_user_by_email(self, email):
-        return self.user_repo.get_by_attribute('email', email)
+    def get_user_by_id(self, user_id):
+        return self.user_repository.get(user_id)
 
     def get_all_users(self):
-        return self.user_repo.get_all()
+        return self.user_repository.get_all()
 
 #Amenity
     def create_amenity(self, amenity_data):
