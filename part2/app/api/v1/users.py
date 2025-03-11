a1b66d3fad7c3877ccc1640c8a69b4ca072774ac
Lucawinwin <windwehrluca@gmail.com>
2025-03-11 12:26:26 +0100

    add task 1

--- part2/app/api/v1/users.py
+++ part2/app/api/v1/users.py
@@ -8,6 +8,7 @@ user_model = api.model('User', {
     'first_name': fields.String(required=True, description='First name of the user'),
     'last_name': fields.String(required=True, description='Last name of the user'),
     'email': fields.String(required=True, description='Email of the user')
+    'password': fields.String(required=True, description='Password for the user')
 })
 
 facade = HBnBFacade()
@@ -27,9 +28,27 @@ class UserList(Resource):
         if existing_user:
             return {'error': 'Email already registered'}, 400
 
+        hashed_password = generate_password_hash(user_data['password'])
+
+        # Create a new user with the hashed password
+        user_data['password'] = hashed_password
+
         new_user = facade.create_user(user_data)
         return {'id': new_user.id, 'first_name': new_user.first_name, 'last_name': new_user.last_name, 'email': new_user.email}, 201
-    
+
+class HBnBFacade:
+    def create_user(self, user_data):
+        # Création de l'utilisateur dans la base de données
+        # Assurez-vous que le mot de passe est stocké sous forme hachée
+        new_user = User(
+            first_name=user_data['first_name'],
+            last_name=user_data['last_name'],
+            email=user_data['email'],
+            password=user_data['password']  # Le mot de passe est déjà haché ici
+        )
+        db.session.add(new_user)
+        db.session.commit()
+        return new_user
 
 @api.route('/<owner_id>')
 class UserResource(Resource):
@@ -40,4 +59,11 @@ class UserResource(Resource):
         user = facade.get_user(owner_id)
         if not user:
             return {'error': 'User not found'}, 404
-        return {'id': user.id, 'first_name': user.first_name, 'last_name': user.last_name, 'email': user.email}, 200
+         user_data = {
+            'id': user.id,
+            'first_name': user.first_name,
+            'last_name': user.last_name,
+            'email': user.email
+        }
+
+        return user_data, 200
