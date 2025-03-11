eb52d13b971d1ce5612bafdf51bf577ef2438274
DQE92 <duquedylan@icloud.com>
2025-03-11 12:42:01 +0100

    user.py

--- part2/app/models/user.py
+++ part2/app/models/user.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-
-
 from .base_model import BaseModel
 import re
-
+import bcrypt
 
 class User(BaseModel):
     def __init__(self, first_name, last_name, email):
@@ -12,13 +10,14 @@ class User(BaseModel):
         self.last_name = last_name
         self.email = email
         self.is_admin = is_admin
+        self.password = None
         
-        def hash_password(self, password):
-    """Hashes the password before storing it."""
-    self.password = bcrypt.generate_password_hash(password).decode('utf-8')
-        def verify_password(self, password):
-    """Verifies if the provided password matches the hashed password."""
-    return bcrypt.check_password_hash(self.password, password)
+    def hash_password(self, password):
+        """Hashes the password before storing it."""
+        self.password = bcrypt.generate_password_hash(password).decode('utf-8')
+    def verify_password(self, password):
+        """Verifies if the provided password matches the hashed password."""
+        return bcrypt.check_password_hash(self.password, password)
 
 @property
 def first_name(self):
