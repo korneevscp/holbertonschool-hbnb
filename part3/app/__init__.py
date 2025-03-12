78293a438f4c5be59b866dcc5fe58717d9a9bcf8
Lucawinwin <windwehrluca@gmail.com>
2025-03-12 10:58:35 +0100

    task 6

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -5,14 +5,19 @@ from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
 from flask_bcrypt import Bcrypt
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
 bcrypt = Bcrypt()
+
 
 
 def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
     app.config.from_object(config_class)
     bcrypt.init_app(app)
+    db.init_app(app)
 
     # Register the users namespace
     api.add_namespace(users_ns, path='/api/v1/users')
