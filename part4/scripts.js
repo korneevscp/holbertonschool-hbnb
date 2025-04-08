64703ff010918d8b4f3e65a5b863dc0ef04d996e
Lucawinwin <windwehrluca@gmail.com>
2025-04-08 12:37:18 +0200

    add css

---
+++ part4/scripts.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+function checkAuthentication() {
+    const token = getCookie('token');
+    const loginLink = document.getElementById('login-link');
+
+    if (!token) {
+        loginLink.style.display = 'block';
+    } else {
+        loginLink.style.display = 'none';
+        // Fetch places data if the user is authenticated
+        fetchPlaces(token);
+    }
+}
+function getCookie(name) {
+    // Function to get a cookie value by its name
+    // Your code here
+}
+}
